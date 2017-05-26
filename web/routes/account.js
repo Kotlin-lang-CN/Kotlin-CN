ee9c5ccf9261a6b2450ac2ddcaa46b4d06087858
yisheng <me@yeshen.org>
2017-05-26 13:17:16 +0800

    change express to vuejs

--- web/routes/account.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET account page. */
-router.get('/', function(req, res, next) {
-    res.render('account', { title: 'Account' });
-});
-
-module.exports = router;
--- web/routes/topics.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET topics page. */
-router.get('/', function(req, res, next) {
-  res.render('topics', { title: 'Topics' });
-});
-
-module.exports = router;
