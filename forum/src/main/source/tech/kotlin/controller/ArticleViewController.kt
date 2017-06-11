49d0e13b833d14a387d8af19fb8ed22c1477aab7
chpengzh <chpengzh@foxmail.com>
2017-06-11 11:57:19 +0800

    修复rss编码错误

--- forum/src/main/source/tech/kotlin/controller/ArticleViewController.kt
+++ forum/src/main/source/tech/kotlin/controller/ArticleViewController.kt
@@ -1,27 +1,39 @@
 package tech.kotlin.controller
 
 import spark.Route
+import tech.kotlin.common.utils.Props
+import tech.kotlin.common.utils.dict
+import tech.kotlin.common.utils.str
 import tech.kotlin.model.domain.Article
 import tech.kotlin.model.domain.Category
 import tech.kotlin.model.domain.TextContent
 import tech.kotlin.model.domain.UserInfo
+import tech.kotlin.model.request.QueryLatestArticleReq
+import tech.kotlin.model.request.QueryTextReq
 import tech.kotlin.model.request.QueryUserReq
 import tech.kotlin.service.account.Users
 import tech.kotlin.service.article.Articles
-import tech.kotlin.model.request.QueryLatestArticleReq
-import tech.kotlin.model.request.QueryTextReq
 import tech.kotlin.service.article.Texts
 import tech.kotlin.utils.Err
 import tech.kotlin.utils.check
-import tech.kotlin.common.utils.dict
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import java.io.StringWriter
+import javax.xml.transform.TransformerFactory
+
 
 /*********************************************************************
  * Created by chpengzh@foxmail.com
  * Copyright (c) http://chpengzh.com - All Rights Reserved
  *********************************************************************/
 object ArticleViewController {
+
+    val properties = Props.loads("project.properties")
+    val cgiHost = properties str "cgi.host"
+
     val getList = Route { req, _ ->
         val offset = req.queryParams("offset")
                 ?.apply { check(Err.PARAMETER) { it.toInt();true } }
@@ -170,36 +182,40 @@ object ArticleViewController {
                 }
             }).result)
         }
-
+        val df = SimpleDateFormat("E, MM dd yyyy HH:mm:ss z", Locale.US)
+        val localeDivide = 14 * 60 * 60 * 1000
+        val doc = DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder()
+                .newDocument().apply {
+            appendChild(createElement("rss").apply {
+                setAttribute("version", "2.0")
+                appendChild(createElement("channel").apply {
+                    appendChild(createElement("title").apply { textContent = "Kotlin-CN 中文网" })
+                    appendChild(createElement("link").apply { textContent = cgiHost })
+                    appendChild(createElement("description").apply { textContent = "社区最新精品文章" })
+                    appendChild(createElement("language").apply { textContent = "${Locale.CHINA}" })
+                    articles.forEach {
+                        appendChild(createElement("item").apply {
+                            appendChild(createElement("title").apply { textContent = it.title })
+                            appendChild(createElement("description").apply {
+                                textContent = contents[it.contentId]!!.content
+                            })
+                            appendChild(createElement("author").apply { textContent = users[it.author]!!.username })
+                            appendChild(createElement("pubDate").apply {
+                                textContent = df.format(it.createTime - localeDivide)
+                            })
+                            appendChild(createElement("link").apply { textContent = "$cgiHost/#/topic/${it.id}" })
+                            appendChild(createElement("guid").apply { textContent = "$cgiHost/#/topic/${it.id}" })
+                        })
+                    }
+                })
+            })
+        }
         resp.header("Content-Type", "application/xml")
-        return@Route """
-    <?xml version='1.0' encoding='UTF-8'?>
-    <rss version="2.0">
-      <channel>
-        <title>Kotlin-CN 中文网</title>
-        <link>http://www.baidu.com</link>
-        <description>社区最新精品文章</description>
-        <language>${Locale.CHINA}</language>
-        ${{
-            val df = SimpleDateFormat("E, MM dd yyyy HH:mm:ss z", Locale.US)
-            val localeDivide = 14 * 60 * 60 * 1000
-            var result = ""
-            articles.forEach {
-                result += """
-                <item>
-                    <title>${it.title}</title>
-                    <description>${contents[it.contentId]!!.content}</description>
-                    <author>${users[it.author]}</author>
-                    <pubDate>${df.format(it.createTime - localeDivide)}</pubDate>
-                    <link>http://www.baidu.com</link>
-                    <guid>http://www.baidu.com/${it.id}</guid>
-                </item>
-                """.trimIndent()
-            }
-            result
-        }()}
-      </channel>
-    </rss>
-    """.trimIndent().trim()
+        return@Route StringWriter().apply {
+            TransformerFactory.newInstance()
+                    .newTransformer()
+                    .transform(DOMSource(doc), StreamResult(this))
+        }.toString()
     }
 }
