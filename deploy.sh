11d2d8e96a2e15df1c29e8401ec561b691874b5f
chpengzh <chpengzh@foxmail.com>
2017-06-12 22:18:12 +0800

    重写测试环境启动和部署脚本

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,17 @@
 cp ../release/project.properties project.properties
 cp project.properties account/src/main/resources && cp project.properties article/src/main/resources
 
-echo "++ compile project(':account') ++"
-./gradlew account:jar
-echo "++ compile project(':article') ++"
-./gradlew article:jar
+echo "++ recompile projects ++"
+./gradlew account:jar && ./gradlew article:jar
+
+echo '++ service stop ++'
+pkill -f *article* && pkill -f *account*
 
 #如果文件夹不存在，创建文件夹
 if [ ! -d "log" ]; then
   mkdir log
 fi
 
-accountPid=$(ps -ef | egrep "account-" | grep -v grep | awk '{print $2}')
-if [ ${accountPid} ]; then
-    kill ${accountPid}
-fi
-articlePid=$(ps -ef | egrep "article-" | grep -v grep | awk '{print $2}')
-if [ ${accountPid} ]; then
-    kill ${accountPid}
-fi
-
 nohup java -jar ./account/build/libs/account-1.0.0-release.jar > log/account.log &
 nohup java -jar ./article/build/libs/article-1.0.0-release.jar > log/article.log &
+echo '++ service restart ++'
